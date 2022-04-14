24692c84233d0497789ce0791f9382cfde321e3a
Sourav Mandal <souravmandal9827@gmail.com>
2022-04-14 09:27:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from myproject import app
+from flask import render_template
+
+@app.route("/")
+def index():
+	return render_template("home.html")
+
+@app.errorhandler(Exception)
+def page_not_found(e):
+    return render_template('404.html')
+
+if __name__=="__main__":
+	app.run(debug=True)
